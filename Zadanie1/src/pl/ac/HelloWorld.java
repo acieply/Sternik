584bd214874fbe3a8fee84af4e13bfceb9032e4c
LGBSPL\acieply <acieply@forefrontcorp.com>
2017-03-11 11:52:41 +0100

    Szkolenie

---
+++ Zadanie1/src/pl/ac/HelloWorld.java
@@ -0,0 +1,11 @@
+package pl.ac;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		//Syso - ctrl + spacja - wyswietla System.out.println()
+		System.out.println();
+	}
+
+}
